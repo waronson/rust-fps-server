5ac5b54caf54e823ca98713feedd2dabb0d1726a
waronson <william.aronson@gmail.com>
2024-02-07 21:03:25 -0600

    Add code

---
+++ src/ws.rs
@@ -0,0 +1,73 @@
+use crate::{Client, Clients};
+use futures::{FutureExt, StreamExt};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use uuid::Uuid;
+use warp::ws::{Message, WebSocket};
+
+pub async fn client_connection(ws: WebSocket, clients: Clients) {
+    println!("establishing client connection... {:?}", ws);
+
+    let (client_ws_sender, mut client_ws_rcv) = ws.split();
+    let (client_sender, client_rcv) = mpsc::unbounded_channel();
+
+    let client_rcv = UnboundedReceiverStream::new(client_rcv);
+
+    tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
+        if let Err(e) = result {
+            println!("error sending websocket msg: {}", e);
+        }
+    }));
+
+    let uuid = Uuid::new_v4().simple().to_string();
+
+    let new_client = Client {
+        client_id: uuid.clone(),
+        state: Vec::new(),
+        sender: Some(client_sender),
+    };
+
+    clients.lock().await.insert(uuid.clone(), new_client);
+
+    while let Some(result) = client_ws_rcv.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                println!("error receiving message for id {}): {}", uuid.clone(), e);
+                break;
+            }
+        };
+        client_msg(&uuid, msg, &clients).await;
+    }
+
+    clients.lock().await.remove(&uuid);
+    println!("{} disconnected", uuid);
+}
+
+async fn client_msg(client_id: &str, msg: Message, clients: &Clients) {
+    println!("received message from {}: {:?}", client_id, msg);
+
+    let data = msg.into_bytes();
+    let mut locked = clients.lock().await;
+    let mut toSend = Vec::new();
+    for client in locked.clone().into_values() {
+        if client.client_id != client_id {
+            toSend.insert(0, client.state);
+        }
+    }
+
+    match locked.get_mut(client_id) {
+        Some(v) => {
+            v.state = data;
+
+            if let Some(sender) = &v.sender {
+                print!("Sending remote state(s)");
+
+                for state in toSend {
+                    let _ = sender.send(Ok(Message::binary(state)));
+                }
+            }
+        }
+        None => return,
+    }
+}
